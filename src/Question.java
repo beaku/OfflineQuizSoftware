3445fa7a1fc27f44c80102a9be5d176c61b6fb47
Re42 <33805964+Re42@users.noreply.github.com>
2018-11-03 14:03:29 +0530

    add basic attributes to question

--- src/Question.java
+++ src/Question.java
@@ -2,5 +2,7 @@ import java.util.Vector;
 
 public class Question {
     private String textContent;
-    private String answer;
+    private String correctAnswer;
+    private Vector<String> options = new Vector<>();
+    private String attemptedAnswer;
 }
