712f6cee9183fe6abac4e894a5dfb763714fa65f
Re42 <33805964+Re42@users.noreply.github.com>
2018-11-06 14:05:39 +0530

    create question generator code (generateSampleQuestions method)

--- src/offlinetestingplatform/Quiz.java
+++ src/offlinetestingplatform/Quiz.java
@@ -1,5 +1,8 @@
 package offlinetestingplatform;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Vector;
 
 public class quiz {
@@ -7,5 +10,18 @@ public class quiz {
 
     public static void main(String[] args) {
         quizInterface quizInterface = new quizInterface();
+    }
+
+    public Vector<question> generateSampleQuestions(){
+
+        Vector<question> questions = new Vector<>();
+
+            for(Integer i = 0; i < 20; i++){
+                ArrayList options = new ArrayList<String>();
+                options.addAll(0, Arrays.asList("option 1","option 2","option 3","option 4"));
+                questions.add(new question("question "+Integer.toString(i), options,i%4+1));
+            }
+
+        return questions;
     }
 }
