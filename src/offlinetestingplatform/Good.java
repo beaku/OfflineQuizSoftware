c5b029ac6ab3cc8c8d7de3cf0063bbee02b189ec
adiagg2000 <33027339+adiagg2000@users.noreply.github.com>
2018-11-06 22:14:48 +0530

    fixes #2

--- src/offlinetestingplatform/Good.java
+++ src/offlinetestingplatform/Good.java
@@ -1,4 +1,14 @@
-//package offlinetestingplatform;
-//
-//public class Good extends question {
-//}
+package offlinetestingplatform;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+public class Good extends question {
+
+  public Good(String _content, ArrayList<String> _options, int _correct) {
+		super(_content, _options, _correct);
+		// TODO Auto-generated constructor stub
+	}
+	private Dictionary options;
+	    private int correctAnswer;
+	    private int selectedAnswer;
+}
